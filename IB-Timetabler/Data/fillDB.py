72fe7b20d6ead1d65987cfcf29171825468a990b
NONAME1103 <57779612+NONAME1103@users.noreply.github.com>
2021-11-06 08:29:09 +0100

    continue fillDB.py

--- IB-Timetabler/Data/fillDB.py
+++ IB-Timetabler/Data/fillDB.py
@@ -29,24 +29,51 @@ print(selectTable("Rooms"))
 print(selectTable("Rooms"))
 connection.close()
 
-classes = """12 IB SL Bio
-12 IB HL Bio Z1
-12 IB HL Bio Z2
-12 IB SL Che
-12 IB HL Che
-12 IB SL Phy
-12 IB HL Phy
-13 IB SL Bio
-13 IB HL Bio Z1
-13 IB HL Bio Z2
-13 IB SL Che
-13 IB HL Che
-13 IB SL Phy
-13 IB HL Phy
-12 IB SL Comp
-12 IB HL Comp
-13 IB SL Comp
-13 IB HL Comp
+classes = """12 SL Bio
+12 HL Bio Z1
+12 HL Bio Z2
+12 SL Che
+12 HL Che
+12 SL Phy
+12 HL Phy
+13 SL Bio
+13 HL Bio Z1
+13 HL Bio Z2
+13 SL Che
+13 HL Che
+13 SL Phy
+13 HL Phy
+12 SL Comp
+12 HL Comp
+13 SL Comp
+13 HL Comp
+12 AI SL Mat
+12 AI SL/HL Mat
+12 AI HL Mat
+12 AA SL Mat
+12 AA SL/HL Mat
+12 AA HL Mat
+13 AI SL Mat
+13 AI SL/HL Mat
+13 AI HL Mat
+13 AA SL Mat 1
+13 AA SL Mat 2
+13 AA SL/HL Mat
+13 AA HL Mat
+12 SL Eng Lang & Lit 1
+12 SL Eng Lang & Lit 2
+12 HL Eng Lang & Lit 3
+12 HL Eng B 1
+12 HL Eng B 2
+12 HL Eng B 3
+13 SL Eng Lit 1
+13 HL Eng Lit 1
+13 HL Eng B 1
+13 HL Eng B 2
+23 SL Geog
+23 HL Geog
+22 SL Geog
+22 HL Geog
 """
 
 numLessons = """6
@@ -67,6 +94,33 @@ numLessons = """6
 4
 6
 4
+6
+6
+4
+6
+6
+4
+6
+6
+4
+6
+6
+6
+4
+6
+6
+10
+10
+10
+10
+6
+10
+10
+10
+6
+4
+6
+4
 """
 years = """12
 12
@@ -78,6 +132,33 @@ years = """12
 13
 13
 13
+13
+13
+13
+13
+12
+12
+13
+13
+12
+12
+12
+12
+12
+12
+13
+13
+13
+13
+13
+13
+13
+12
+12
+12
+12
+12
+12
 13
 13
 13
@@ -106,4 +187,31 @@ C1
 C1
 C1
 C1
+M6
+M4
+M4
+M1
+M2
+M2
+M5
+M4
+M4
+M2
+M3
+M1
+M1
+E3
+E5
+E1
+E7
+E2
+E4
+E1
+E6
+E4
+E5
+H2
+H2
+H4
+H1
 """
