4f45f3a7c8e58e1e34635adfed198a8a299f5120
NONAME1103 <faparicio1103@gmail.com>
2021-11-08 02:06:00 +0100

    continue

--- IB-Timetabler/Data/fillDB.py
+++ IB-Timetabler/Data/fillDB.py
@@ -29,7 +29,19 @@ print(selectTable("Rooms"))
 print(selectTable("Rooms"))
 connection.close()
 
-classes = """12 SL Bio
+
+class Lesson:
+
+    def __init__(self, name, year, numLessons, teacher, blocks, group):
+        self.Name = name
+        self.Year = year
+        self.NumLessons = numLessons
+        self.Teacher = teacher
+        self.Blocks = blocks
+        self.Group = group
+
+
+names = """12 SL Bio
 12 HL Bio Z1
 12 HL Bio Z2
 12 SL Che
@@ -74,6 +86,181 @@ classes = """12 SL Bio
 23 HL Geog
 22 SL Geog
 22 HL Geog
+23 SL His
+23 HL His
+22 SL His
+22 HL His
+23 SL Econ
+23 HL Econ
+22 SL Econ
+22 HL Econ
+23 SL A BUSMAN
+23 HL A BUSMAN
+23 SL B BUSMAN
+23 HL B BUSMAN
+22 SL S3B BUSMAN
+22 SL S3C BUSMAN
+22 HL S3B BUSMAN
+BTEC Y12
+BTEC Y13
+PPS Y12
+PPS Y13
+23 SL GloPo
+23 HL GloPo
+22 SL 223S GloPo
+22 HL 223S GloPo
+23 SL Psy
+23 HL Psy
+22 SL 22S3 Psy
+22 HL 22S3 Psy
+FRE B 12 SL
+FRE B 12 HL
+FRE B 13 SL
+FRE AI SL
+SPA B 12 SL
+SPA B 12 HL
+SPA 12 LD
+SPA 12 AI SL
+SPA B 13 SL
+SPA B 13 HL
+SPA 13 LD
+12 SL SEHS
+12 HL SEHS
+13 SL SEHS
+13 HL SEHS
+GER LL 12Z1 SL
+GER LL 12Z2 SL
+GER LL 12 HL
+GER LL 13Z1 SL
+GER LL 13Z2 SL
+GER LL 13 HL
+GER B 12 Z1 SL
+GER B 12 Z2 SL
+GER B 12 HL
+GER AI 12 Z1
+GER B 13 Z1 SL
+GER B 13 Z2 SL
+GER B 13 HL
+GER AI 13 Z1
+12 SL Visual Arts
+12 HL Visual Arts
+13 SL Visual Arts
+13 HL Visual Arts
+12 SL Theatre
+12 HL Theatre
+13 SL Theatre
+13 HL Theatre
+"""
+
+years = """12
+12
+12
+12
+12
+12
+12
+13
+13
+13
+13
+13
+13
+13
+12
+12
+13
+13
+12
+12
+12
+12
+12
+12
+13
+13
+13
+13
+13
+13
+13
+12
+12
+12
+12
+12
+12
+13
+13
+13
+13
+12
+12
+13
+13
+12
+12
+13
+13
+12
+12
+13
+13
+12
+12
+12
+12
+13
+13
+13
+12
+13
+12
+13
+12
+12
+13
+13
+12
+12
+13
+13
+12
+12
+13
+13
+12
+12
+12
+12
+13
+13
+13
+12
+12
+13
+13
+12
+12
+12
+13
+13
+13
+12
+12
+12
+12
+13
+13
+13
+13
+12
+12
+13
+13
+12
+12
+13
+13
 """
 
 numLessons = """6
@@ -121,52 +308,70 @@ numLessons = """6
 4
 6
 4
-"""
-years = """12
-12
-12
-12
-12
-12
-12
-13
-13
-13
-13
-13
-13
-13
-12
-12
-13
-13
-12
-12
-12
-12
-12
-12
-13
-13
-13
-13
-13
-13
-13
-12
-12
-12
-12
-12
-12
-13
-13
-13
-13
-12
-12
-13
-13
+6
+4
+6
+4
+6
+4
+6
+4
+6
+4
+6
+4
+6
+6
+4
+14
+14
+4
+4
+6
+4
+6
+4
+6
+4
+6
+4
+6
+4
+6
+6
+6
+4
+2
+6
+6
+4
+2
+6
+4
+6
+4
+6
+6
+4
+6
+6
+4
+6
+6
+4
+6
+6
+6
+4
+6
+6
+4
+6
+4
+6
+4
+6
+4
 """
 
 teachers = """S8
@@ -214,4 +419,137 @@ H2
 H2
 H4
 H1
+H7
+H5
+H6
+H6
+H10
+H10
+H10
+H10
+H8
+H8
+H10
+H9
+H8
+H9
+H8
+H9
+H8
+H9
+H8
+H1
+H1
+H5
+H5
+H12
+H12
+H11
+H11
+L1
+L3
+L3
+L1
+L6
+L6
+L3
+L5
+L6
+L6
+L3
+P2
+P3
+P1
+P3
+G2
+G3
+G2
+G2
+G3
+G2
+G7
+G4
+G7
+G8
+G7
+G4
+G7
+G5
+A3
+A3
+A4
+A4
+A1
+A1
+A1
+A1
 """
+
+# Just did math below. Continue with English
+
+optionBlocks = """4
+4
+4
+6
+6
+4
+4
+4
+4
+4
+6
+6
+4
+4
+6
+6
+6
+6
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+"""
+
+groups = """SL
+HL
+HL
+SL
+HL
+SL
+HL
+SL
+HL
+HL
+SL
+HL
+SL
+HL
+SL
+HL
+SL
+HL
+SL
+HL
+HL
+SL
+HL
+HL
+SL
+HL
+HL
+SL
+SL
+SL
+HL
+"""
+
